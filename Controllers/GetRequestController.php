64061926e7abe805d03c29f279f637e048a4ae4e
Harry <harrywinser@hotmail.co.uk>
2014-02-03 23:14:06 +0000

    Completed the Get Request Controller. Needs refactoring however.

--- Controllers/GetRequestController.php
+++ Controllers/GetRequestController.php
@@ -1,26 +1,70 @@
 <?php
 
+// We can cache this by looking at the request, and then store it in a text file. 
+// check age of text file against the age of the XML file. If younger, serve it, if not, then delete and refresh.
+
+require_once '../Models/RegionsModel.php';
+require_once '../Models/CountriesModel.php';
+require_once '../Models/FurtherStatisticsModel.php';
+require_once '../Models/AreasModel.php';
+
+$countryModel = new CountriesModel();
+$regionModel = new RegionsModel();
+$fStatsModel = new FurtherStatisticsModel();
+$areasModel = new AreasModel();
+
+$responseXML = new DOMDocument();
+$base = $responseXML->createElement("reponse");
+
+$base->setAttribute("timestamp", date("YmdHi"));
+$crime = $responseXML->createElement("crimes");
+$crime->setAttribute("year", "6-2013");
+
 if (isset($_GET["region"])) {
-    $regionNamesasdas = $_GET["region"]; // this needs to be changed to the safer version
+    $givenRegionName = $_GET["region"];
+    // Need to check for Action Fraud/Transport
+    
+    if (strtolower($givenRegionName) === "british_transport_police" || strtolower($givenRegionName) === "action_fraud") {
+        $obj = $fStatsModel->getFurtherStatisticsByName($givenRegionName); // gets the object. We then do something similar to the below.
+        
+        // This needs to be all chanegd, as this comes through as a region... Apprently?!
+        
+        $furtehrStatNode = $responseXML->createElement("national");
+        $furtehrStatNode->setAttribute("id", $furtherStat->getName());
+        $furtehrStatNode->setAttribute("total", $furtherStat->getTotal());
+        
+        $crime->appendChild($furtehrStatNode);
+        
+    } else {
+        $region = $regionModel->getRegionByName($givenRegionName);
 
-    echo $regionNamesasdas . "</br>";
+        $name = $region->getName();
+
+        $regionNode = $responseXML->createElement("region");
+        $regionNode->setAttribute("id", $name);
+        $regionNode->setAttribute("total", $region->getTotal());
+
+        $areaNames = $region->getAreaNames();
+        foreach ($areaNames as $areaName) {
+
+            $areaObj = $areasModel->getAreaByName($areaName);
+
+            $areaNode = $responseXML->createElement("area");
+            $areaNode->setAttribute("id", $areaObj->getName());
+            $areaNode->setAttribute("total", $areaObj->getTotal());
+            $regionNode->appendChild($areaNode);
+        }
+
+        $crime->appendChild($regionNode);
+    }
 } else {
-    require_once '../Models/RegionsModel.php';
-    require_once '../Models/CountriesModel.php';
 
-    $countryModel = new CountriesModel();
-    $regionModel = new RegionsModel();
+    // this is now done for XML. Woo!
+    $regions = $regionModel->getAllRegions();
+    $countries = $countryModel->getAllCounties();
+    $fStats = $fStatsModel->getAllFurtherStatistics();
 
-    $regions = $regionModel->getAllRegions(); // gets all regions correctly.
 
-    $countries = $countryModel->GetAllCounties();
-
-    $responseXML = new DOMDocument();
-    $base = $responseXML->createElement("reponse");
-
-    $base->setAttribute("timestamp", date("YmdHi"));
-    $crime = $responseXML->createElement("crime");
-    $crime->setAttribute("year", "6-2013");
 
     foreach ($regions as $region) {
         $name = $region->getName();
@@ -30,23 +74,29 @@ if (isset($_GET["region"])) {
         $regionNode->setAttribute("total", $region->getTotal());
         $crime->appendChild($regionNode);
     }
-    
-    foreach($countries as $country){
+
+    foreach ($countries as $country) {
         $name = $country->getName();
         $countryNode = $responseXML->createElement(strtolower($name));
         $countryNode->setAttribute("total", $country->getTotal());
-        
+
         $crime->appendChild($countryNode);
     }
-    
-    
-    
 
-    header("Content-type: text/xml");
-    $base->appendChild($crime);
-    $responseXML->appendChild($base);
-    echo $responseXML->saveXML();
+    foreach ($fStats as $stat) {
+        $statNode = $responseXML->createElement("national");
+        $statNode->setAttribute("id", $stat->getName());
+        $statNode->setAttribute("total", $stat->getTotal());
+        $crime->appendChild($statNode);
+    }
+
+
+
+    //Could i not just do an include as a view?
 }
 
 
-
+header("Content-type: text/xml");
+$base->appendChild($crime);
+$responseXML->appendChild($base);
+echo $responseXML->saveXML();
