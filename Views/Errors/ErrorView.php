516502df7dd9c8f3bdc3821dfb537d782337b580
Harry <harrywinser@hotmail.co.uk>
2014-02-09 18:20:03 +0000

    Added error messages Removed some old test data

---
+++ Views/Errors/ErrorView.php
@@ -0,0 +1,18 @@
+<?php
+
+$errorMessage = $_SESSION["errorMessage"];
+$errorCode = $_SESSION["errorCode"];
+
+$responseXML = new DOMDocument();
+$base = $responseXML->createElement("reponse");
+$base->setAttribute("timestamp", date("YmdHi"));
+
+$errorNode = $responseXML->createElement("error");
+$errorNode->setAttribute("code", $errorCode);
+$errorNode->setAttribute("desc", $errorMessage);
+
+$base->appendChild($errorNode);
+$responseXML->appendChild($base);
+
+header("Content-type: text/xml");
+echo $responseXML->saveXML();
