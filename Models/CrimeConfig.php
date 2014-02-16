4ad905b84ab18b5d2823a58799d86a4870c90e62
Harry <harrywinser@hotmail.co.uk>
2014-02-16 21:49:10 +0000

    Added new functions to access more data

--- Models/CrimeConfig.php
+++ Models/CrimeConfig.php
@@ -1,58 +1,65 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of CrimeConfig
- *
- * @author Harry
- */
 class CrimeConfig {
-    
+
     private $configXml, $xpath;
-    
+
     public function __construct() {
         $this->configXml = new DOMDocument();
         $this->configXml->load("../Config/CrimeConfig.xml");
         $this->xpath = new DOMXpath($this->configXml);
     }
-    
-    public function GetCrimeName($abName){
+
+    public function GetCrimeName($abName) {
         $crimeNode = $this->_getNodeOnAbName($abName);
-        
+
         return $crimeNode->getAttribute("name");
     }
-    
-    public function CheckIfCrimeCategory($abName){
+
+    public function CheckIfCrimeCategory($abName) {
         $crimeNode = $this->_getNodeOnAbName($abName);
-        $isTrue = $crimeNode->getAttribute("iscrimecatagory");
-        if($crimeNode->getAttribute("iscrimecatagory") === "true"){ // not finding the node.. Balls!
+        if ($crimeNode->getAttribute("iscrimecatagory") === "true") { // not finding the node.. Balls!
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
-    
-    public function GetCrimeCatagory($abName){
+
+    public function GetCrimeCatagory($abName) {
         $crimeNode = $this->_getNodeOnAbName($abName);
-        
+
         return $crimeNode->getAttribute("crimecatagory");
     }
-    
-    public function GetCrimeType($abName){
+
+    public function GetCrimeType($abName) {
         $crimeNode = $this->_getNodeOnAbName($abName);
         return $crimeNode->getAttribute("type");
     }
-    
-    private function _getNodeOnAbName($abName){
-        return $this->xpath->query("CrimeAbriviations/Crime [@abrivated='".$abName."']")->item(0);
+
+    public function getAllCrimeNames() {
+        $crimes = $this->configXml->getElementsByTagName("Crime");
+        $crimeNames = array();
+
+        foreach ($crimes as $crime) {
+            $crimeNames[] = $crime->getAttribute("name");
+        }
+
+        return $crimeNames;
     }
-    
+
+    public function getAllCrimeNamesAbrivated() {
+        $crimes = $this->configXml->getElementsByTagName("Crime");
+        $crimeNames = array();
+
+        foreach ($crimes as $crime) {
+            $crimeNames[] = $crime->getAttribute("abrivated");
+        }
+
+        return $crimeNames;
+    }
+
+    private function _getNodeOnAbName($abName) {
+        return $this->xpath->query("CrimeAbriviations/Crime [@abrivated='" . $abName . "']")->item(0);
+    }
 
 }
